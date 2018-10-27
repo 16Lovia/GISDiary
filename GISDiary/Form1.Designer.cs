6f4f1ac98eae356e8d6cf40c02c41e064f7bcc54
jessieyang567 <2500876951@qq.com>
2018-10-27 16:55:46 +0800

    tt

--- GISDiary/Form1.Designer.cs
+++ GISDiary/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(648, 359);
+            this.label1.Location = new System.Drawing.Point(639, 360);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 15);
             this.label1.TabIndex = 1;
