8a3ef774b3eb9f118da8f02a434f6f48c0cfcc4c
Lovia <lcy980517@163.com>
2018-10-28 21:08:37 +0800

    10.28lcy

--- GISDiary/Form1.cs
+++ GISDiary/Form1.cs
@@ -6,6 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.NetworkAnalysis;
+using ESRI.ArcGIS.Display;
+
 
 namespace GISDiary
 {
@@ -13,7 +20,43 @@ namespace GISDiary
     {
         public Form1()
         {
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
             InitializeComponent();
+            string filepath = "C:\\Users\\lcy\\Desktop\\GISDairy\\China.mxd";
+            axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerArrowHourglass;         
+            axMapControl1.LoadMxFile(filepath);
+            axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerDefault;
+            // loadMapDocument();
+        }
+
+        private void loadMapDocument()
+        {
+            OpenFileDialog openFileDialog;
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "打开地图文档";
+            openFileDialog.Filter = "map document(*.mxd)|*.mxd";
+            openFileDialog.ShowDialog();
+            string filepath = openFileDialog.FileName;
+            if (axMapControl1.CheckMxFile(filepath))
+            {
+                axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerArrowHourglass;
+                axMapControl1.LoadMxFile(filepath);
+
+                axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerDefault;
+            }
+            else
+            {
+                MessageBox.Show(filepath + "不是有效的地图文档");
+            }
+        }
+
+        private void axMapControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent e)
+        {
+            if (e.button == 1)
+                this.axMapControl1.Extent = this.axMapControl1.TrackRectangle();
+            else if (e.button == 2)
+                this.axMapControl1.Pan();
+            label1.Text = " 当前坐标 X = " + e.mapX.ToString() + " Y = " + e.mapY.ToString() + " " + this.axMapControl1.MapUnits.ToString().Substring(4);
         }
     }
 }
