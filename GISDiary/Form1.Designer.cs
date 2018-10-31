bdf356349d0eb6ff3c6d8bb4c8deb13adba612db
Rivalei <961095446@qq.com>
2018-10-31 10:25:59 +0800

    network_lei(erro:net cann't be recognized) 操作说明：鼠标点击选点-按钮开启路径规划

--- GISDiary/Form1.Designer.cs
+++ GISDiary/Form1.Designer.cs
@@ -31,31 +31,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
             this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
             this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
             this.label1 = new System.Windows.Forms.Label();
             this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.axSymbologyControl1 = new ESRI.ArcGIS.Controls.AxSymbologyControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_net = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSymbologyControl1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(392, 8);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // axLicenseControl1
             // 
             this.axLicenseControl1.Enabled = true;
             this.axLicenseControl1.Location = new System.Drawing.Point(1, 8);
+            this.axLicenseControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axLicenseControl1.Name = "axLicenseControl1";
             this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
             this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
@@ -64,67 +59,105 @@
             // axToolbarControl1
             // 
             this.axToolbarControl1.Location = new System.Drawing.Point(39, 8);
+            this.axToolbarControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axToolbarControl1.Name = "axToolbarControl1";
             this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(265, 28);
+            this.axToolbarControl1.Size = new System.Drawing.Size(397, 28);
             this.axToolbarControl1.TabIndex = 2;
             // 
             // axMapControl1
             // 
             this.axMapControl1.Location = new System.Drawing.Point(39, 62);
+            this.axMapControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axMapControl1.Name = "axMapControl1";
             this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
-            this.axMapControl1.Size = new System.Drawing.Size(677, 344);
+            this.axMapControl1.Size = new System.Drawing.Size(1015, 516);
             this.axMapControl1.TabIndex = 3;
             this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(657, 13);
+            this.label1.Location = new System.Drawing.Point(986, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             // 
             // axTOCControl1
             // 
             this.axTOCControl1.Location = new System.Drawing.Point(731, 62);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(4);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(265, 344);
+            this.axTOCControl1.Size = new System.Drawing.Size(397, 516);
             this.axTOCControl1.TabIndex = 5;
+            // 
+            // axSymbologyControl1
+            // 
+            this.axSymbologyControl1.Location = new System.Drawing.Point(396, 0);
+            this.axSymbologyControl1.Name = "axSymbologyControl1";
+            this.axSymbologyControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSymbologyControl1.OcxState")));
+            this.axSymbologyControl1.Size = new System.Drawing.Size(11, 8);
+            this.axSymbologyControl1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_net
+            // 
+            this.btn_net.Location = new System.Drawing.Point(1090, 13);
+            this.btn_net.Name = "btn_net";
+            this.btn_net.Size = new System.Drawing.Size(110, 42);
+            this.btn_net.TabIndex = 7;
+            this.btn_net.Text = "网络分析";
+            this.btn_net.UseVisualStyleBackColor = true;
+            this.btn_net.Click += new System.EventHandler(this.btn_net_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 434);
+            this.ClientSize = new System.Drawing.Size(1512, 651);
+            this.Controls.Add(this.btn_net);
+            this.Controls.Add(this.axSymbologyControl1);
             this.Controls.Add(this.axTOCControl1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.axMapControl1);
             this.Controls.Add(this.axToolbarControl1);
             this.Controls.Add(this.axLicenseControl1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSymbologyControl1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
         private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
         private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
         private System.Windows.Forms.Label label1;
         private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private ESRI.ArcGIS.Controls.AxSymbologyControl axSymbologyControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_net;
     }
 }
 
