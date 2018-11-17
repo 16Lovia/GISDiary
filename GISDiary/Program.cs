22f2ab586e62f554f6534b138e5d9e37a28da39e
jessieyang567 <2500876951@qq.com>
2018-11-17 14:51:28 +0800

    yang_03

--- GISDiary/Program.cs
+++ GISDiary/Program.cs
@@ -15,7 +15,7 @@ namespace GISDiary
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormStart());
+            Application.Run(new Form2());
         }
     }
 }
